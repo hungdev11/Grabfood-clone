1ab192fd19100db0d1b2934020b277a25b39606c
HUNG <hungphangialai11@gmail.com>
2025-04-18 16:46:20 +0700

    UPDATE: integrate vnpay

--- BE/src/main/java/com/api/service/PaymentService.java
+++ BE/src/main/java/com/api/service/PaymentService.java
@@ -2,6 +2,7 @@ package com.api.service;
 
 import com.api.dto.request.CreateOrderRequest;
 import com.api.dto.response.OrderResponse;
+import jakarta.servlet.http.HttpServletRequest;
 
 import java.math.BigDecimal;
 
@@ -9,8 +10,9 @@ public interface PaymentService {
     void updatePaymentStatus(String paymentCode, String status);
 
     void createPayment(BigDecimal amount, Long orderId, String code);
-
+    void createPaymentVNPay(BigDecimal amount, Long orderId, String code);
     OrderResponse createOrderPaymentCod(CreateOrderRequest request);
 
     String createOrderPaymentMomo(CreateOrderRequest request);
+    String createOrderPaymentVNPay(CreateOrderRequest orderRequest, HttpServletRequest request);
 }
