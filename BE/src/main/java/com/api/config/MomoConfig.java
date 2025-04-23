5dcf20f9e46b04bdfc6b29283c7f5c3ce082a458
PHUC LE <lhphuc.2410@gmail.com>
2025-04-23 18:56:11 +0700

    init branch

--- BE/src/main/java/com/api/config/MomoConfig.java
+++ BE/src/main/java/com/api/config/MomoConfig.java
@@ -4,11 +4,11 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MomoConfig {
-    public static final String PARTNER_CODE = "MOMO"; // Thay bằng Partner Code của bạn
-    public static final String ACCESS_KEY = "F8BBA842ECF85";     // Thay bằng Access Key
-    public static final String SECRET_KEY = "K951B6PE1waDMi640xX08PD3vg6EkVlz";     // Thay bằng Secret Key
-    public static final String ENDPOINT = "https://test-payment.momo.vn/v2/gateway/api/create"; // Test endpoint
-    public static final String REDIRECT_URL = "http://localhost:6969/grab/payments/momo/callback"; // URL trả về
-    public static final String IPN_URL = "https://4121-2001-ee0-4fc7-2110-2c18-216c-7358-9791.ngrok-free.app/grab/payments/momo/notify";
+    public static final String PARTNER_CODE = "MOMO";
+    public static final String ACCESS_KEY = "F8BBA842ECF85";
+    public static final String SECRET_KEY = "K951B6PE1waDMi640xX08PD3vg6EkVlz";
+    public static final String ENDPOINT = "https://test-payment.momo.vn/v2/gateway/api/create";
+    public static final String REDIRECT_URL = "http://localhost:6969/grab/payments/momo/callback";
+    public static final String IPN_URL = "http://localhost:6969/grab/grab/payments/momo/notify";
 
 }
