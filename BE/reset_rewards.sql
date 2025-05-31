bb42e6e69b10873243114e778e0a3f5515f9c594
BiiDenn <batho09082003@gmail.com>
2025-05-31 13:43:09 +0700

    done api reward

--- BE/reset_rewards.sql
+++
@@ -1,9 +0,0 @@
--- Reset tất cả reward claims (để test lại)
-DELETE FROM shipper_reward;
-
--- Hoặc chỉ reset cho một shipper cụ thể
--- DELETE FROM shipper_reward 
--- WHERE shipper_id = (SELECT id FROM shipper WHERE phone = '0987654321');
-
--- Kiểm tra sau khi reset
-SELECT COUNT(*) as remaining_claims FROM shipper_reward; 
